5761b9cb8607376831dffd8cafd1fb15e7ee1392
oneeljilc <joneel@ucsd.edu>
2025-10-16 22:16:37 -0600

    Update global.js

--- global.js
+++ global.js
@@ -4,7 +4,7 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-
+// Step: 2
 // const navLinks = $$('nav a');
 
 // let currentLink = navLinks.find(
@@ -12,3 +12,21 @@ function $$(selector, context = document) {
 // );
 
 // currentLink?.classList.add('current');
+
+//Step 3:
+let pages = [
+    { url: '', title: 'Home'},
+    { url: 'projects/', title: 'Projects'},
+    { url: 'CV/', title: 'CV'},
+    { url: 'contact/', title: 'Contact'},
+]
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    nav.insertAdjacentHTML('beforeend', `<a href="/${url}">${title}</a>`);
+}
