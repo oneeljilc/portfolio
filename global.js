deb6775683f0ed4b5f23d074d1d87814f0df24ec
oneeljilc <joneel@ucsd.edu>
2025-10-16 21:57:09 -0600

    Update global.js

--- global.js
+++ global.js
@@ -3,3 +3,12 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+
+const navLinks = $$('nav a');
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+currentLink?.classList.add('current');
