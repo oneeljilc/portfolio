a5c7027264080bfafd9e657c87b849394e87e546
oneeljilc <joneel@ucsd.edu>
2025-10-18 14:40:15 -0600

    Update global.js

--- global.js
+++ global.js
@@ -93,3 +93,18 @@ select.addEventListener('input', function (event) {
 
   console.log('color scheme changed to', value);
 });
+
+// Lab 4: Step 1.2
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
