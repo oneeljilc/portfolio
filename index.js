2bceebb4946d95766ebf8f165f663187bd8e6cca
oneeljilc <joneel@ucsd.edu>
2025-10-18 21:00:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
+
+renderProjects(latestProjects, projectsContainer, 'h2');
