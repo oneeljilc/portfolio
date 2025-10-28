a195a03cc1e36db843c2076cfc72b1cc03937455
oneeljilc <joneel@ucsd.edu>
2025-10-27 21:51:00 -0600

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -15,12 +15,36 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 });
 
-let arc = d3.arc().innerRadius(0).outerRadius(50)({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
+// Lab 5 Step 1.3
+//let arc = d3.arc().innerRadius(0).outerRadius(50)({
+//  startAngle: 0,
+//  endAngle: 2 * Math.PI,
+//});
+//d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+//Lab 5 Step 1.4
+let data = [1, 2];
+let total = 0;
+for (let d of data) {
+  total += d;
+}
+let angle = 0;
+let arcData = [];
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+const svg = d3.select('#projects-pie-plot');
+
+arcs.forEach((arc, i) => {
+  svg.append('path')
+  .attr('d', arc)
+  .attr('fill', 'red')
+});
 
 // Lab 4 Step 1.3 "Check Your Understanding"
 //
