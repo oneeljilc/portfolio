0f8b1b797bfa32dec7b07a4b79938241b32eb3b8
oneeljilc <joneel@ucsd.edu>
2025-11-03 23:32:07 -0700

    Lab 6 Step 2

--- meta/main.js
+++ meta/main.js
@@ -1,19 +1,10 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-// Read in csv file
-//async function loadData() {
-//  const data = await d3.csv('loc.csv');
-//  console.log(data);
-//  return data;
-//}
-
-//let data = await loadData();
-
 // Row conversion function (from Lab 6 Step 1.1)
 async function loadData() {
   const data = await d3.csv('loc.csv', (row) => ({
     ...row,
-    line: Number(row.line), // or just +row.line
+    line: Number(row.line),
     depth: Number(row.depth),
     length: Number(row.length),
     date: new Date(row.date + 'T00:00' + row.timezone),
@@ -43,8 +34,6 @@ function processCommits(data) {
 
       Object.defineProperty(ret, 'lines', {
         value: lines,
-        // What other options do we need to set?
-        // Hint: look up configurable, writable, and enumerable
         writable: true,
         configurable: true,
         enumerable: false,
@@ -55,37 +44,30 @@ function processCommits(data) {
 }
 
 function renderCommitInfo(data, commits) {
-  // Create the dl element
   const dl = d3.select('#stats').append('dl').attr('class', 'stats');
 
-  // Add total LOC
   dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
   dl.append('dd').text(data.length);
 
-  // Add total commits
   dl.append('dt').text('Total commits');
   dl.append('dd').text(commits.length);
 
-  // Number of unique files in the codebase
   const uniqueFiles = new Set(data.map(d => d.file)).size;
   dl.append('dt').text('Files in codebase');
   dl.append('dd').text(uniqueFiles);
 
-  // Average line length (characters)
   const avgLength = d3.mean(data, d => d.length).toFixed(1);
   dl.append('dt').text('Average line length');
   dl.append('dd').text(`${avgLength} chars`);
 
-  // Longest line length
   const maxLength = d3.max(data, d => d.length);
   dl.append('dt').text('Longest line');
   dl.append('dd').text(`${maxLength} chars`);
 
-  // Day-of-week
   const dayCounts = d3.rollup(
     commits,
     v => v.length,
-    d => d.datetime.getDay() // 0 = Sunday
+    d => d.datetime.getDay()
   );
 
   const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
@@ -94,11 +76,73 @@ function renderCommitInfo(data, commits) {
 
   dl.append('dt').text('Most active day');
   dl.append('dd').text(days[topDayIndex]);
-
-
 }
 
-let data = await loadData();
-let commits = processCommits(data);
+// --- Scatter plot ---
+function renderScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 20, right: 20, bottom: 40, left: 50 };
+
+  const usableArea = {
+    left: margin.left,
+    right: width - margin.right,
+    top: margin.top,
+    bottom: height - margin.bottom,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`);
+
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([usableArea.left, usableArea.right]);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 24])
+    .range([usableArea.bottom, usableArea.top]);
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale).tickFormat((d) => `${String(d % 24).padStart(2, '0')}:00`);
+
+  svg.append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left},0)`)
+    .call(
+      d3.axisLeft(yScale)
+        .tickFormat('')
+        .tickSize(-usableArea.width)
+    );
+
+  svg.append('g')
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue')
+    .attr('opacity', 0.7);
+
+  svg.append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  svg.append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+}
+
+// --- Main execution (moved to bottom) ---
+const data = await loadData();
+const commits = processCommits(data);
 
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
+
